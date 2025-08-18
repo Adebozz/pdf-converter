63ade3b5fc2d7293f381bf61806f4a89747bd72b
Adebozz <ademolaadeigbe3@gmail.com>
2025-08-18 21:21:49 +0100

    `Update Sparkles component and utils library with various configuration changes`

--- my-app/lib/utils.ts
+++ my-app/lib/utils.ts
@@ -1,7 +1,6 @@
-// src/lib/utils.ts
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: any[]) {
+export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
